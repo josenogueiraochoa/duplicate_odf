86bf618a3f4a3a91186469035202de18d5d69452
Rafa Gaioso <rrgaioso@gmail.com>
2015-04-28 14:12:39 +0200

    Novas dimensións para as imaxes modificadas

--- convert_logo.sh
+++ convert_logo.sh
@@ -1,5 +1,5 @@
 
 for i in $(ls $1/*);
 do
-	convert -resize x136 -background white -gravity west -extent 743x136 $i $i.new
+	convert -resize x136 -background white -gravity west -extent 940x136 $i $i.new
 done
