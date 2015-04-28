8f95124ccd65866f6ad7a5ce71c34559d0951fc4
Rafa Gaioso <rrgaioso@gmail.com>
2015-04-28 12:50:53 +0200

    Engadida a opción para traballar con dúas imaxes

--- script.sh
+++ script.sh
@@ -17,11 +17,21 @@ if [ ! -f $1.bak ]; then
 	cp $1 $1.bak
 fi
 
+rm Pictures/*
 # Extraemos a imaxe que está dentro do modelo base
 unzip -o $1 "Pictures/*"
 
 # Gardamos o nome que ten a imaxe interna 
-NOME=$(ls Pictures)
+#NOME=$(ls Pictures)
+
+ls Pictures/ > files.txt
+NOME=$(head files.txt)
+
+if [ $2 == 1 ]; then
+	NOME=$(head -1 files.txt)
+else
+	NOME=$(head -2 files.txt | tail -1 files.txt)
+fi
 
 # Agora temos que percorrer as imaxes do cartafol Logos e crear un novo ODF con cada unha delas
 for image in logos/*
@@ -30,9 +40,12 @@ do
 
 	zip $1 Pictures/$NOME
 	
-	imagename=$(basename "$image")
-	echo $imagename
+	#imagename=$(basename "$image")
+	#echo $imagename
 
 	cp $1 finais/$(basename "$image")-$1
 
 done
+
+cp $1.bak $1
+rm files.txt
